b5a656d97a6a53d20a99125baf496a735fbc5541
kwchun0520 <ckw05200@gmail.com>
2024-08-03 13:40:09 +0800

    updated

--- schedule.sh
+++ schedule.sh
@@ -4,12 +4,18 @@ REGION=us-east-1
 
 aws events put-rule \
 --name my-schedule-rule-for-$1 \
---schedule-expression 'cron(0 13 * * ? *)'
+--schedule-expression 'cron(0 4 * * ? *)'
 
+
+if [ "$2" == "update" ]; then
+    aws lambda remove-permission \
+    --function-name $1 \
+    --statement-id my-scheduled-event-for-$1 
+fi
 
 aws lambda add-permission \
 --function-name $1 \
---statement-id my-scheduled-event \
+--statement-id my-scheduled-event-for-$1 \
 --action 'lambda:InvokeFunction' \
 --principal events.amazonaws.com \
 --source-arn arn:aws:events:$REGION:$AWS_ACCOUNT_ID:rule/my-schedule-rule-for-$1
